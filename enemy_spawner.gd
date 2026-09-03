a8e8a2b16509972b813628fa8f392a2e1e1f1076
Rubinho <martinillo114@gmail.com>
2026-09-03 20:49:22 +0200

    feat: add basic enemy spawning

---
+++ enemy_spawner.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+@export var enemy_scene: PackedScene
+
+func spawn_enemy():
+	var enemy= enemy_scene.instantiate()
+	enemy.position = Vector2(40, 30)
+	get_parent().add_child.call_deferred(enemy)
+	
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	spawn_enemy()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
