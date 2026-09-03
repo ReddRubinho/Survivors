5f1165b1e1a7ca13e6df1cb04f427fbdeeaf5cdc
Rubinho <martinillo114@gmail.com>
2026-09-03 20:10:45 +0200

    feat: add player movement

--- player.gd
+++ player.gd
@@ -1,6 +1,9 @@
 extends CharacterBody2D
-
-
+@export var speed: float = 200.0
+func _physics_process(_delta):
+	var direction = Input.get_vector("Move_Left", "Move_Right", "Move_Up", "Move_Down")
+	velocity = direction * speed
+	move_and_slide()
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
